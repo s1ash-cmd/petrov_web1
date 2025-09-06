5e648d6efc2c15586bd2ce5b6e1014d97fe74baa
s1ash <dimapetrov200567@gmail.com>
2025-09-06 16:18:09 +0300

    6 done

--- petrov_web1/Models/Movie.cs
+++ petrov_web1/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace petrov_web1.Models
 {
@@ -6,9 +8,11 @@ namespace petrov_web1.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
